45d233bbad63a386960d5eeeb606e64bdf6780bc
eet-fuk <dutch109@gmail.com>
2012-01-11 23:47:14 +0100

    Better random spam analysis

--- ddc_process.py
+++ ddc_process.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import hashlib
+
 
 VERSION = 1
 
 
 def is_spam(domain):
-  return len(domain.strip())%2 > 0
+  # returns dummy result, but consistent for a domain
+  hasher = hashlib.md5()
+  hasher.update(domain.encode("utf-8"))
+  return hasher.digest()[0] > 127
